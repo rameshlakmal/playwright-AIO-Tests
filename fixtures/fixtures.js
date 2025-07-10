c30fc48dfcab21551aacf4a68090bcd75ce1687b
Ramesh <66841859+rameshlakmal@users.noreply.github.com>
2025-07-10 12:14:40 +0530

    refactor: remove unused CommonAction import and associated extension

--- fixtures/fixtures.js
+++ fixtures/fixtures.js
@@ -1,16 +1,10 @@
 import { test as base } from "@playwright/test";
-import CommonAction from "../utils/commonactions";
 import LoginPage from "../pages/LoginPage.js";
 
 export const test = base.extend({
   loginTest: async ({ page }, use) => {
     const loginTest = new LoginPage(page);
     await use(loginTest);
-  },
-
-  commonActions: async ({ page }, use) => {
-    const commonActions = new CommonAction(page);
-    await use(commonActions);
   },
 });
 
