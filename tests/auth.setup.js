e3386a22d43b5c7e1273ce72797966a2823d225c
Ramesh <66841859+rameshlakmal@users.noreply.github.com>
2025-07-10 13:55:09 +0530

    refactor: update test setup name to include test case identifier

--- tests/auth.setup.js
+++ tests/auth.setup.js
@@ -1,7 +1,7 @@
 import { test as setup, expect } from "../fixtures/fixtures.js";
 import { Login } from "../test-data/test-data.json";
 
-setup("User login", async ({ loginTest, page }) => {
+setup("@CCS-TC-2: User login", async ({ loginTest, page }) => {
   await loginTest.login(Login.username, Login.password);
   await expect(page).toHaveURL("https://www.saucedemo.com/v1/inventory.html");
   await page.context().storageState({ path: "./.auth/user.json" });
